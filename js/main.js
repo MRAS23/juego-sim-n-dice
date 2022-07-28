965f34be5325f4d185291013359943c604fb89f4
MRAS23 <101474625+MRAS23@users.noreply.github.com>
2022-07-28 19:20:32 -0300

    agrega funcion reproduce secuencia
    es para que cada vez que se aprete jugar agregue un elemento nuevo al final del vector que corresponde a la secuencia que debe imitar el jugador

--- js/main.js
+++ js/main.js
@@ -5,3 +5,31 @@ function generaNumeroAleatorio() {
 }
 
 console.log(generaNumeroAleatorio());
+
+let secuencia = [];
+
+function reproduceSecuenciaPC() {
+  let nuevoElementoSecuencia = generaNumeroAleatorio();
+
+  /* secuencia.push(nuevoElementoSecuencia);
+  1:rojo 2:azul 3:verde 4:amarillo */
+
+  if (nuevoElementoSecuencia === 1) {
+    secuencia.push("rojo");
+  } else if (nuevoElementoSecuencia === 2) {
+    secuencia.push("azul");
+  } else if (nuevoElementoSecuencia === 3) {
+    secuencia.push("verde");
+  } else if (nuevoElementoSecuencia === 4) {
+    secuencia.push("amarillo");
+  }
+
+  return secuencia;
+}
+
+document.querySelector("#boton-comenzar").onclick = function (event) {
+  reproduceSecuenciaPC();
+  console.log(secuencia);
+
+  event.preventDefault();
+};
