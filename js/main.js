cd5ce0d414edf03004de8f511935af25ce31ee3c
Mauro Adamo <101474625+MRAS23@users.noreply.github.com>
2022-07-28 21:24:07 -0300

    Crear funcion secuencia (#3)
    * agrega funcion reproduce secuencia
    
    es para que cada vez que se aprete jugar agregue un elemento nuevo al final del vector que corresponde a la secuencia que debe imitar el jugador
    
    * obtiene elementos botones
    
    se obtiene los elementos botones y se agrega un event listener para saber si fueron clickeados. Luego con esto se va a hacer una funcion que obtenga la secuencia de colores que va a ingresar el usuario usando estos botones
    
    * agrega funcion genera patron jugador
    
    va guardando el patron que el jugador ingresa en un array y luego va a servir parar compararlo con el patron generado por el pc y ver si coincide

--- js/main.js
+++ js/main.js
@@ -5,3 +5,63 @@ function generaNumeroAleatorio() {
 }
 
 console.log(generaNumeroAleatorio());
+
+let secuenciaPC = [];
+
+function generaSecuenciaPC() {
+  let nuevoElementoSecuenciaPC = generaNumeroAleatorio();
+
+  /* secuencia.push(nuevoElementoSecuencia);
+  1:rojo 2:azul 3:verde 4:amarillo */
+
+  if (nuevoElementoSecuenciaPC === 1) {
+    secuenciaPC.push("rojo");
+  } else if (nuevoElementoSecuenciaPC === 2) {
+    secuenciaPC.push("azul");
+  } else if (nuevoElementoSecuenciaPC === 3) {
+    secuenciaPC.push("verde");
+  } else if (nuevoElementoSecuenciaPC === 4) {
+    secuenciaPC.push("amarillo");
+  }
+
+  return secuenciaPC;
+}
+
+document.querySelector("#boton-comenzar").onclick = function (event) {
+  generaSecuenciaPC();
+  console.log(secuenciaPC);
+
+  event.preventDefault();
+};
+
+let secuenciaJugador = [];
+
+let $cuadradoRojo = document.querySelector(".rojo");
+let $cuadradoAzul = document.querySelector(".azul");
+let $cuadradoVerde = document.querySelector(".verde");
+let $cuadradoAmarillo = document.querySelector(".amarillo");
+$cuadradoRojo.addEventListener("click", () => generaSecuenciaJugador(1));
+$cuadradoAzul.addEventListener("click", () => generaSecuenciaJugador(2));
+$cuadradoVerde.addEventListener("click", () => generaSecuenciaJugador(3));
+$cuadradoAmarillo.addEventListener("click", () => generaSecuenciaJugador(4));
+
+function generaSecuenciaJugador(color) {
+  if (color === 1) {
+    secuenciaJugador.push("rojo");
+  } else if (color === 2) {
+    secuenciaJugador.push("azul");
+  } else if (color === 3) {
+    secuenciaJugador.push("verde");
+  } else if (color === 4) {
+    secuenciaJugador.push("amarillo");
+  }
+
+  return false;
+}
+
+/* $cuadradoRojo.addEventListener("click", () => clickeado(1));
+function clickeado(color) {
+  if (color === 1) {
+    secuenciaJugador.push("rojo");
+  }
+} */
