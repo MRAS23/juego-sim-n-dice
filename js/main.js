221e8ef2e2feb394f0bb6eb3b41eb695a31a0b08
MRAS23 <101474625+MRAS23@users.noreply.github.com>
2022-07-28 21:21:10 -0300

    agrega funcion genera patron jugador
    va guardando el patron que el jugador ingresa en un array y luego va a servir parar compararlo con el patron generado por el pc y ver si coincide

--- js/main.js
+++ js/main.js
@@ -6,44 +6,62 @@ function generaNumeroAleatorio() {
 
 console.log(generaNumeroAleatorio());
 
-let secuencia = [];
+let secuenciaPC = [];
 
-function reproduceSecuenciaPC() {
-  let nuevoElementoSecuencia = generaNumeroAleatorio();
+function generaSecuenciaPC() {
+  let nuevoElementoSecuenciaPC = generaNumeroAleatorio();
 
   /* secuencia.push(nuevoElementoSecuencia);
   1:rojo 2:azul 3:verde 4:amarillo */
 
-  if (nuevoElementoSecuencia === 1) {
-    secuencia.push("rojo");
-  } else if (nuevoElementoSecuencia === 2) {
-    secuencia.push("azul");
-  } else if (nuevoElementoSecuencia === 3) {
-    secuencia.push("verde");
-  } else if (nuevoElementoSecuencia === 4) {
-    secuencia.push("amarillo");
+  if (nuevoElementoSecuenciaPC === 1) {
+    secuenciaPC.push("rojo");
+  } else if (nuevoElementoSecuenciaPC === 2) {
+    secuenciaPC.push("azul");
+  } else if (nuevoElementoSecuenciaPC === 3) {
+    secuenciaPC.push("verde");
+  } else if (nuevoElementoSecuenciaPC === 4) {
+    secuenciaPC.push("amarillo");
   }
 
-  return secuencia;
+  return secuenciaPC;
 }
 
 document.querySelector("#boton-comenzar").onclick = function (event) {
-  reproduceSecuenciaPC();
-  console.log(secuencia);
+  generaSecuenciaPC();
+  console.log(secuenciaPC);
 
   event.preventDefault();
 };
 
-contadorClicks = 0;
+let secuenciaJugador = [];
+
 let $cuadradoRojo = document.querySelector(".rojo");
 let $cuadradoAzul = document.querySelector(".azul");
 let $cuadradoVerde = document.querySelector(".verde");
 let $cuadradoAmarillo = document.querySelector(".amarillo");
-$cuadradoRojo.addEventListener("click", clickeado);
-$cuadradoAzul.addEventListener("click", clickeado);
-$cuadradoVerde.addEventListener("click", clickeado);
-$cuadradoAmarillo.addEventListener("click", clickeado);
+$cuadradoRojo.addEventListener("click", () => generaSecuenciaJugador(1));
+$cuadradoAzul.addEventListener("click", () => generaSecuenciaJugador(2));
+$cuadradoVerde.addEventListener("click", () => generaSecuenciaJugador(3));
+$cuadradoAmarillo.addEventListener("click", () => generaSecuenciaJugador(4));
 
-function clickeado() {
-  contadorClicks++;
+function generaSecuenciaJugador(color) {
+  if (color === 1) {
+    secuenciaJugador.push("rojo");
+  } else if (color === 2) {
+    secuenciaJugador.push("azul");
+  } else if (color === 3) {
+    secuenciaJugador.push("verde");
+  } else if (color === 4) {
+    secuenciaJugador.push("amarillo");
+  }
+
+  return false;
 }
+
+/* $cuadradoRojo.addEventListener("click", () => clickeado(1));
+function clickeado(color) {
+  if (color === 1) {
+    secuenciaJugador.push("rojo");
+  }
+} */
